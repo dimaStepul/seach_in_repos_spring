0eaaf1e51c030bdb2460cf7c5efd0dbcca0be9bc
Dmitrii Stepul <trushaka2001@gmail.com>
2024-04-19 22:04:28 +0300

    added base for tests

--- src/test/java/org/example/spring_task/SpringTaskApplicationTests.java
+++ src/test/java/org/example/spring_task/SpringTaskApplicationTests.java
@@ -1,13 +1,29 @@
 package org.example.spring_task;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
-class SpringTaskApplicationTests {
+@AutoConfigureMockMvc
+class GitHubControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
 
   @Test
-  void contextLoads() {
+  void testGetRepos() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/form")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("organization", "dmitrii239")
+            .param("accessToken", "2281337239")
+            .param("targetWord", "hello"))
+        .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+        .andExpect(MockMvcResultMatchers.view().name("unauthorized_error.html"));
   }
-
 }
